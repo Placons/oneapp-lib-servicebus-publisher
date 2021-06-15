c49c84f2a8738efa3c9262371da5e546d5f92c1a
nstergiou-prodyna <nikos.stergiou@prodyna.com>
2021-06-15 12:24:58 +0300

    Restructured test helper to be accessed from other services

--- test/integration_test.go
+++ test/integration_test.go
@@ -1,28 +1,16 @@
 package test
 
 import (
-	"crypto/tls"
-	"fmt"
-	"log"
-	"net"
-	"net/http"
-	"net/http/httptest"
-	"sync"
 	"testing"
 
 	publisher "github.com/Placons/oneapp-lib-servicebus-publisher/v2"
+	testpublisher "github.com/Placons/oneapp-lib-servicebus-publisher/v2/integration"
 	"github.com/Placons/oneapp-logger/logger"
 	"github.com/stretchr/testify/assert"
 )
 
-var sa []*httptest.Server
-var (
-	requests      map[string]int
-	requestsMutex = sync.RWMutex{}
-)
-
 func TestShouldPublishMessage(t *testing.T) {
-	publisherMockServer := MockPublisher(t, "test-queue")
+	publisherMockServer := testpublisher.MockPublisher(t, "test-queue", "7000")
 	defer publisherMockServer.Close()
 
 	l := logger.NewStandardLogger("oneapp-my-service")
@@ -33,14 +21,14 @@ func TestShouldPublishMessage(t *testing.T) {
 		SharedKeyName: "my-shared-key",
 		SigningKey:    "my-signing-key",
 	}
-	p := publisher.NewPublisher(l, HttpClientNoCertVerify(), c)
+	p := publisher.NewPublisher(l, testpublisher.HttpClientNoCertVerify(), c)
 
 	err := p.Publish("some message")
 	assert.NoError(t, err)
 }
 
 func TestReturnErrorPublishMessageFails(t *testing.T) {
-	publisherMockServer := MockPublisher(t, "test-queue")
+	publisherMockServer := testpublisher.MockPublisher(t, "test-queue", "7000")
 	defer publisherMockServer.Close()
 
 	l := logger.NewStandardLogger("oneapp-my-service")
@@ -51,46 +39,8 @@ func TestReturnErrorPublishMessageFails(t *testing.T) {
 		SharedKeyName: "my-shared-key",
 		SigningKey:    "my-signing-key",
 	}
-	p := publisher.NewPublisher(l, HttpClientNoCertVerify(), c)
+	p := publisher.NewPublisher(l, testpublisher.HttpClientNoCertVerify(), c)
 
 	err := p.Publish("some message")
 	assert.Error(t, err)
-}
-
-func MockPublisher(t *testing.T, queue string) *httptest.Server {
-	publisherServeMux := http.NewServeMux()
-	publisherServeMux.HandleFunc(fmt.Sprintf("/%s/messages", queue), func(res http.ResponseWriter, req *http.Request) {
-		assert.Equal(t, http.MethodPost, req.Method)
-		res.WriteHeader(http.StatusCreated)
-	})
-
-	cert, err := tls.LoadX509KeyPair("testdata/cert/server.crt", "testdata/cert/server.key")
-	if err != nil {
-		t.Errorf("Bad server certs, error was: %s", err)
-	}
-	certs := []tls.Certificate{cert}
-
-	publisherMockServer := httptest.NewUnstartedServer(publisherServeMux)
-	publisherMockServer.TLS = &tls.Config{Certificates: certs}
-	publisherMockServer.Listener.Close()
-	publisherMockServer.Listener = createListener("7000")
-	publisherMockServer.StartTLS()
-
-	return publisherMockServer
-}
-
-func HttpClientNoCertVerify() *http.Client {
-	return &http.Client{Transport: &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	}}
-}
-
-func createListener(port string) net.Listener {
-	l, err := net.Listen("tcp", "127.0.0.1:"+port)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return l
 }
